37f82960a8b6e796d6bd9e1cfff44c2fdb109fed
coderrabbie <golamrabbi.dv@gmail.com>
2022-10-27 22:24:33 +0600

    port is successfully worked

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 const categories = require("./data/categories.json");
