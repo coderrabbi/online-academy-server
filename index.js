22af294013c599b176fd019419e3bc1c09fd899b
coderrabbie <golamrabbi.dv@gmail.com>
2022-12-06 16:48:19 +0600

    link change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 const categories = require("./data/categories.json");
